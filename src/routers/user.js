35b1c475bd2ed4a6338156e7557f29526d9aa3ae
rmahawewa <rmahawewa@yahoo.com>
2025-06-13 15:39:47 +0530

    update user feed function by using mongoose methods: , in aggregate

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,60 +4,132 @@ const Image = require("../models/photos");
 const PhotoReaction = require("../models/photo-reaction");
 const { userAuth } = require("../middleware/auth");
 
+// userRouter.get("/feed", async (req, res) => {
+// 	try {
+// 		const loggedUser = "6841738705c90d15f9f0b308";
+// 		const categoriesObjectArray = await Image.find({
+// 			uploadedUserId: loggedUser,
+// 		}).select("category");
+// 		const categoriesArray = new Set();
+// 		categoriesObjectArray.forEach((categ) => {
+// 			categ.category.forEach((c) => {
+// 				categoriesArray.add(c);
+// 			});
+// 		});
+// 		const releventImages = await Image.find({
+// 			category: { $in: Array.from(categoriesArray) },
+// 		});
+// 		const imageIds = new Array();
+// 		releventImages.forEach((img) => {
+// 			imageIds.push(img._id);
+// 		});
+// 		const releventReactions = await PhotoReaction.find({
+// 			photoId: { $in: Array.from(imageIds) },
+// 		});
+// 		const like = new Array();
+// 		const familier = new Array();
+// 		const aTrue = new Array();
+// 		const love = new Array();
+// 		const wonderful = new Array();
+// 		const iFeelJelousy = new Array();
+// 		releventReactions.forEach((r) => {
+// 			if (r.reactionType === "like") like.push(r);
+// 			if (r.reactionType === "familier") familier.push(r);
+// 			if (r.reactionType === "aTrue") aTrue.push(r);
+// 			if (r.reactionType === "love") love.push(r);
+// 			if (r.reactionType === "wonderful") wonderful.push(r);
+// 			if (r.reactionType === "iFeelJelousy") iFeelJelousy.push(r);
+// 		});
+
+// 		res.json({
+// 			imageData: releventImages,
+// 			like: like,
+// 			familier: familier,
+// 			aTrue: aTrue,
+// 			love: love,
+// 			wonderful: wonderful,
+// 			iFeelJelousy: iFeelJelousy,
+// 		});
+// 	} catch (err) {
+// 		console.error(err.message);
+// 	}
+// });
+
 userRouter.get("/feed", async (req, res) => {
 	try {
 		const loggedUser = "6841738705c90d15f9f0b308";
-		const categoriesObjectArray = await Image.find({
+		// Find categories for images uploaded by the logged user
+		const distinctCategories = await Image.distinct("category", {
 			uploadedUserId: loggedUser,
-		}).select("category");
-		console.log(categoriesObjectArray);
-		const categoriesArray = new Set();
-		categoriesObjectArray.forEach((categ) => {
-			categ.category.forEach((c) => {
-				categoriesArray.add(c);
-			});
 		});
-		console.log(categoriesArray);
-		const releventImages = await Image.find({
-			category: { $in: Array.from(categoriesArray) },
+		//Find relevent images and their reactions using aggregation
+		const feedData = await Image.aggregate([
+			{ $match: { category: { $in: distinctCategories } } },
+			// Lookup reactions for these images
+			{
+				$lookup: {
+					from: "photoreactions",
+					localField: "_id",
+					foreignField: "photoId",
+					as: "reactions",
+				},
+			},
+			//Project the required fields and restructure data for the client
+			{
+				$project: {
+					_id: 1,
+					uploadedUserId: 1,
+					fileName: 1,
+					url: 1,
+					photoTitle: 1,
+					category: 1,
+					photoDescription: 1,
+					//photo-reaction fields
+					reactions: {
+						_id: 1,
+						reactionType: 1,
+						reactedBy: 1,
+					},
+				},
+			},
+		]);
+		//Process reactions to get counts an seperate arrays
+		const allReactions = [];
+		feedData.forEach((image) => {
+			if (image.reactions) {
+				allReactions.push(...image.reactions);
+			}
 		});
-		// console.log(releventImages);
-		const imageIds = new Array();
-		releventImages.forEach((img) => {
-			// console.log(img._id);
-			imageIds.push(img._id);
-		});
-		console.log(imageIds);
-		const releventReactions = await PhotoReaction.find({
-			photoId: { $in: Array.from(imageIds) },
-		});
-		const like = new Array();
-		const familier = new Array();
-		const aTrue = new Array();
-		const love = new Array();
-		const wonderful = new Array();
-		const iFeelJelousy = new Array();
-		releventReactions.forEach((r) => {
-			console.log(r.reactionType === "wonderful");
+		const like = [];
+		const familier = [];
+		const aTrue = [];
+		const love = [];
+		const wonderful = [];
+		const iFeelJelousy = [];
+
+		allReactions.forEach((r) => {
 			if (r.reactionType === "like") like.push(r);
-			if (r.reactionType === "familier") familier.push(r);
-			if (r.reactionType === "aTrue") aTrue.push(r);
-			if (r.reactionType === "love") love.push(r);
-			if (r.reactionType === "wonderful") wonderful.push(r);
-			if (r.reactionType === "iFeelJelousy") iFeelJelousy.push(r);
+			else if (r.reactionType === "familier") familier.push(r);
+			else if (r.reactionType === "aTrue") aTrue.push(r);
+			else if (r.reactionType === "love") love.push(r);
+			else if (r.reactionType === "wonderful") wonderful.push(r);
+			else if (r.reactionType === "iFeelJelousy") iFeelJelousy.push(r);
 		});
 
 		res.json({
-			imageData: releventImages,
-			like: like,
-			familier: familier,
-			aTrue: aTrue,
-			love: love,
-			wonderful: wonderful,
-			iFeelJelousy: iFeelJelousy,
+			imageData: feedData.map(({ reactions, ...imageFields }) => imageFields),
+			reactions: {
+				like: like,
+				familier: familier,
+				true: aTrue,
+				love: love,
+				wonderful: wonderful,
+				iFeelJelousy: iFeelJelousy,
+			},
 		});
 	} catch (err) {
-		console.error(err.message);
+		console.log(err.message);
+		res.status(400).send(err.message);
 	}
 });
 
