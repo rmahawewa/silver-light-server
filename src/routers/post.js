0a7c13b7ece204a2cb1ba79ca7d9e52cbf4e4856
rmahawewa <rmahawewa@yahoo.com>
2025-06-15 18:50:54 +0530

    update post

--- src/routers/post.js
+++ src/routers/post.js
@@ -4,7 +4,7 @@ const Post = require("../models/post");
 const { userAuth } = require("../middleware/auth");
 const { validatePostPhotoData } = require("../utils/validation");
 
-postRouter.post("/save", userAuth, async (req, res) => {
+postRouter.post("/post/save", userAuth, async (req, res) => {
 	try {
 		const loggedInuser = req.user;
 		const { images, title, description, category } = req.body;
@@ -21,6 +21,29 @@ postRouter.post("/save", userAuth, async (req, res) => {
 		}
 		res.json({ message: "Post successfully saved", data: savedPost });
 	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
+
+postRouter.patch("/post/update", userAuth, async (req, res) => {
+	try {
+		const loggedInUser = req.user;
+		const { _id, createdUserId, images, title, description } = req.body;
+		if (loggedInUser !== createdUserId) {
+			throw new Error("Permission denied");
+		}
+		validatePostPhotoData(req);
+		const post = await Post.findById({ _id: _id });
+		post.title = title;
+		post.photos = images;
+		post.description = description;
+		const updatedPost = await post.save();
+		if (updatedPost) {
+			await updatedPost.addCategoriesFromImages();
+		}
+		res.json({ message: "Post successfully updated", data: updatedPost });
+	} catch (err) {
+		console.error(err.message);
 		res.status(400).send(err.message);
 	}
 });
