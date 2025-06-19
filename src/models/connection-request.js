b18be4a9567517feb96985d79dd173634672996a
rmahawewa <rmahawewa@yahoo.com>
2025-06-19 12:02:34 +0530

    create ConnectionRequest model

--- src/models/connection-request.js
+++ src/models/connection-request.js
@@ -25,4 +25,20 @@ const connectionRequestSchema = new mongoose.Schema(
 );
 
 //indexing the schema to find the relevent record easily
-connectionRequestSchema.index({});
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
+connectionRequestSchema.pre("save", function (next) {
+	const connectionRequest = this;
+	//Check if the fromUserId is same as toUserId
+	if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+		throw new Error("Cannot send connection request to yourself");
+	}
+	next();
+});
+
+const ConnectionRequestModel = new mongoose.model(
+	"ConnectionRequest",
+	connectionRequestSchema
+);
+
+module.exports = ConnectionRequestModel;
