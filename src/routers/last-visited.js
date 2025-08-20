4575126fd240767add9987d267737c4fefe08d98
rmahawewa <rmahawewa@yahoo.com>
2025-08-20 21:18:53 +0530

    get last visited time by userId and visitedURL

--- src/routers/last-visited.js
+++ src/routers/last-visited.js
@@ -7,17 +7,33 @@ lastVisitedRouter.post("/lastvisited/save", userAuth, async (req, res) => {
 	try {
 		const loggedUserId = req.user._id;
 		const { siteUrl } = req.body;
-		const LastVisited = new LastVisited({
+		const lastVisited = new LastVisited({
 			userId: loggedUserId,
 			visitedURL: siteUrl,
 		});
-		const lastvisited = await LastVisited.save();
+		const lastvisitedSaved = await lastVisited.save();
 		res.json({
 			message: "Page visited data saved successfully",
-			data: lastvisited,
+			data: lastvisitedSaved,
 		});
 	} catch (err) {
 		res.status(400).send(err.message);
+	}
+});
+
+lastVisitedRouter.post("/lastvisited/get", userAuth, async (req, res) => {
+	try {
+		const { loggedInUsr, siteUrl } = req.body;
+		const lastVisitedDateTime = await LastVisited.find({
+			userId: loggedInUsr,
+			visitedURL: siteUrl,
+		})
+			.sort({ createdAt: -1 })
+			.limit(1);
+
+		res.json({ data: lastVisitedDateTime });
+	} catch (err) {
+		res.status("400").send(err.message);
 	}
 });
 
